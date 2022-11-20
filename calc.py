7516834f3e10a609a3ac82fc1a9d83ff4a2fb3c7
lasted <shihabmohamed04@gmail.com>
2022-11-20 15:45:10 +0000

    QoL update
    Added limit to amount of digits displayed on screen.
    Added exception handling to eval() function.

--- calc.py
+++ calc.py
@@ -118,10 +118,14 @@ class Calculator:
         self.total_expression += self.current_expression
         self.update_total_label()
         
-        self.current_expression = str(eval(self.total_expression))
+        try:
+            self.current_expression = str(eval(self.total_expression))
 
-        self.total_expression = ""
-        self.update_label()
+            self.total_expression = ""
+        except Exception as e:
+            self.current_expression = "Error"
+        finally:
+            self.update_label()
 
     def create_equals_button(self):
         button = tk.Button(self.buttons_frame, text="=", bg=LIGHT_BLUE,
@@ -134,10 +138,14 @@ class Calculator:
         return frame
 
     def update_total_label(self):
-        self.total_label.config(text=self.total_expression)
+        expression = self.total_expression
+        for operator,symbol in self.operations.items():
+            expression = expression.replace(operator, f' {symbol} ')
+        self.total_label.config(text=expression)
+
 
     def update_label(self):
-        self.label.config(text=self.current_expression)
+        self.label.config(text=self.current_expression[:11])
 
     def run(self):
         self.window.mainloop()
